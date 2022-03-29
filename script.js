0f43c3f8c76335dd948f05847b381e4141f5a487
Andrey Esman <andreyesman.md@gmil.com>
2022-03-29 12:40:54 +0300

    Задание к уроко 2

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
-const goods = [
-  { title: "Shirt", price: 150 },
-  { title: "Socks", price: 50 },
-  { title: "Jacket", price: 150 },
-  { title: "Shoes", price: 250 },
-];
-
-const renderGoodsItem = (product) => {
-  return `<div class='goods-item'>
-          <img src="img/websiteplanet-dummy-360X400.png">
-          <h3>${product.title}</h3>
-          <p>${product.price}</p>
+// Создаем класс
+class GoodsItem {
+  constructor(product, img = "img/websiteplanet-dummy-360X400.png") {
+    this.title = product.title;
+    this.price = product.price;
+    this.img = img;
+  }
+  render() {
+    return `<div class='goods-item'>
+          <img src=${this.img}>
+          <h3>${this.title}</h3>
+          <p>${this.price}</p>
           <button class=" btn btn-buy">Купить</button>
           </div>`;
-};
+  }
+}
 
-const page = (x) => {
-  document.querySelector(".goods-list").innerHTML = x
-    .map((product) => renderGoodsItem(product))
-    .join("");
-};
-page(goods);
+class GoodsList {
+  constructor(container = ".goods-list") {
+    this.goods = [];
+    this.container = container;
+    this.fetchGoods();
+    this.allGoods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: "Shirt", price: 150 },
+      { title: "Socks", price: 50 },
+      { title: "Jacket", price: 350 },
+      { title: "Shoes", price: 250 },
+    ];
+  }
+  getSum() {
+    let priceOfGoods = 0;
+    this.goods.forEach((item) => {
+      priceOfGoods += item.price;
+    });
+  }
+  renderGoods() {
+    let block = document.querySelector(this.container); // обращаемся к контейнеру
+
+    for (let product of this.goods) {
+      let productObj = new GoodsItem(product);
+      this.allGoods.push(productObj);
+      block.insertAdjacentHTML("beforeend", productObj.render());
+    }
+  }
+}
+
+let list = new GoodsList();
+list.renderGoods();
+list.getSum();
+
+class Basket {
+  //добавляем товары
+  addGood() {}
+  //удаляем товар
+  removeGood() {}
+  //Замена товара в корзине
+  changeGood() {}
+  //Отрисовываем корзину
+  renderBasket() {}
+}
+
+class ElemBasket {
+  //отрисовываем отдельный элемент корзины
+  render() {}
+}
